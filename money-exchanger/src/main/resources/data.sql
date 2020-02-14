55b1650e43426928b3cd088bbad9fadc0abd319c
Maduranga <mg.madugamage@gmail.com>
2020-02-14 18:16:44 +0800

    Uncomment data.sql

--- money-exchanger/src/main/resources/data.sql
+++ money-exchanger/src/main/resources/data.sql
@@ -1,16 +1,16 @@
---DROP TABLE IF EXISTS currency_rates;
---
---CREATE TABLE currency_rates (
---  currecy VARCHAR(250) NOT NULL,
---  buy DOUBLE NOT NULL,
---  sell DOUBLE NOT NULL
---);
---
---
---insert into currency_rates values('USD', '1.3392','1.3574');
---insert into currency_rates  values('HKD', '0.1738','0.1698');
+DROP TABLE IF EXISTS currency_rates;
+
+CREATE TABLE currency_rates (
+  currecy VARCHAR(250) NOT NULL,
+  buy DOUBLE NOT NULL,
+  sell DOUBLE NOT NULL
+);
 
 
---CREATE TABLE transactions (
---
---);
+insert into currency_rates values('USD', '1.3392','1.3574');
+insert into currency_rates  values('HKD', '0.1738','0.1698');
+
+
+CREATE TABLE transactions (
+
+);
